ee19ae712d4ad55a32cf045ca80c0dbf29f90a9f
钱珺 <jy8239778@126.com>
2014-07-11 21:10:22 +0800

    no message

--- h4_picture.js
+++ h4_picture.js
@@ -125,9 +125,9 @@ function showcomment()
 		var thetime=commentJson.comment[i].time.slice(0,4)+'年'+commentJson.comment[i].time.slice(5,7)+'月'+commentJson.comment[i].time.slice(8,10)+'日';
 		var thiscomment= '<div class = "one_comment"><div class = "comment-left"><img src="'
 		               + commentJson.comment[i].headportrait +'" ><p class = "comment-info">' 
-					   + commentJson.comment[i].headportrait.name  
-					   + '</p></div><div class = "comment-right"><p class = "comment-info">'
-					   + commentJson.comment[i].floor + '楼    ' + thetime+'</p><p class="comment-text">' + '</p></div>';
+					   + commentJson.comment[i].name + '</p></div><div class = "comment-right"><p class = "comment-info">'
+					   + commentJson.comment[i].floor + '楼    ' + thetime+'</p><p class="comment-text">' 
+					   + commentJson.comment[i].comment + '</p></div>';
 		$('#comments').append(thiscomment);
 	}
 }
